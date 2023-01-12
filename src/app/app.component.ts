39c6613ae4f6b755a15c500b9f1ba9e4025e65ac
Kenny Molina <kjmolina@live.com>
2023-01-12 12:34:01 -0600

    feat: :tada:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-app';
+  disabled = true;
+  age = 50;
+  names = ['kenny', 'jared', 'molina', 'murillo'];
+  enable() {
+    this.disabled = !this.disabled;
+  }
+  add() {
+    this.age = this.age + 1;
+  }
+  onscroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.title = element.value;
+  }
 }
